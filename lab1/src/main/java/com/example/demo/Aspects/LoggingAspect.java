68eb7eb54739689c28839895f154c77dd5647cc4
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2025-03-04 23:16:28 +0200

    fixed issue with aspectj dependencies

--- lab1/src/main/java/com/example/demo/Aspects/LoggingAspect.java
+++ lab1/src/main/java/com/example/demo/Aspects/LoggingAspect.java
@@ -1,21 +1,15 @@
-//package com.example.demo.Aspects;
-//
-//import org.aspectj.lang.annotation.*;
-//import org.aspectj.lang.JoinPoint;
-//import org.springframework.stereotype.Component;
-//
-//@Aspect
-//@Component
-//public class LoggingAspect {
-//
-//    @Before("execution(* com.example.demo.controllers.Controller.*(..))")
-//    public void logBefore(JoinPoint joinPoint) {
-//        System.out.println("🔵 Starting execution of: " + joinPoint.getSignature().getName());
-//    }
-//
-//    @After("execution(* com.example.demo.controllers.Controller.*(..))")
-//    public void logAfter(JoinPoint joinPoint) {
-//        System.out.println("🟢 Finished execution of: " + joinPoint.getSignature().getName());
-//    }
-//}
-//
+package com.example.demo.Aspects;
+
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.JoinPoint;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* com.example.demo.controllers.Controller.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("🔵 Starting execution of: " + joinPoint.getSignature().getName());
+    }
+}
